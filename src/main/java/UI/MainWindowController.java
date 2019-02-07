c395ea9a3110490e1e7a45424e29351e9036c04d
Nick Mironov <nickfrom22nd@gmail.com>
2019-02-07 21:30:27 +0400

    Added a pushing of file or directory into the server

--- src/main/java/UI/MainWindowController.java
+++ src/main/java/UI/MainWindowController.java
@@ -13,7 +13,9 @@ import models.File;
 import models.User;
 import services.FileService;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.sql.Timestamp;
 import java.util.*;
 
@@ -46,13 +48,8 @@ public class MainWindowController {
 
     private User user;
 
-    void setUser(User user) {
-        this.user = user;
-
+    private void updateFilesTable() {
         FileService fileService = new FileService(user.getID(), user.getPassword());
-
-        usernameLabel.setText("Files of " + user.getName());
-
         List<File> userFiles = fileService.getAll();
         TreeItem<FileRow> root = new TreeItem<>(new FileRow("n", new Timestamp(0)));
         fileNameColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<FileRow, String> param) -> param.getValue().getValue().getNameProperty());
@@ -80,6 +77,13 @@ public class MainWindowController {
         filesTree.setShowRoot(false);
     }
 
+    void setUser(User user) {
+        this.user = user;
+        usernameLabel.setText("Files of " + user.getName());
+
+        updateFilesTable();
+    }
+
     public void addFileClicked(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/AddFileDialog.fxml"));
         Parent root = loader.load();
@@ -91,6 +95,64 @@ public class MainWindowController {
         stage.setResizable(false);
         stage.showAndWait();
 
-        System.out.println(controller.filePathInput.getText());
+        String files_list = controller.filePathInput.getText();
+        String[] files_path = files_list.split(",");
+
+        List<File> files;
+
+        for (String path : files_path) {
+            java.io.File temp_file = new java.io.File(path);
+            if (!temp_file.exists()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Wrong file");
+                alert.setHeaderText("Wrong file");
+                alert.setContentText("File " + path + " does not exist!");
+                alert.showAndWait();
+                return;
+            }
+
+            if (temp_file.isFile()) {
+                insertFileIntoDB(temp_file, path);
+                updateFilesTable();
+            }
+
+            if (temp_file.isDirectory()) {
+                insertCatalogIntoDB(temp_file);
+                updateFilesTable();
+            }
+        }
+    }
+
+    private void insertFileIntoDB(java.io.File dirFile, String path) throws IOException {
+        String name = path.substring(path.lastIndexOf('/')+1);
+        String location = path.substring(0, path.lastIndexOf('/'));
+
+        byte[] content = Files.readAllBytes(dirFile.toPath());
+
+        File file = new File(
+                name,
+                location,
+                content,
+                Timestamp.from(
+                        Files.readAttributes(
+                                dirFile.toPath(),
+                                BasicFileAttributes.class).creationTime().toInstant()
+                )
+        );
+
+        FileService fileService = new FileService(user.getID(), user.getPassword());
+        fileService.save(file);
+    }
+
+    private void insertCatalogIntoDB(java.io.File catalog) throws IOException {
+        java.io.File[] filesInDir = catalog.listFiles();
+        for (java.io.File dirFile : filesInDir) {
+            if (dirFile.isDirectory()) {
+                insertCatalogIntoDB(dirFile);
+            } else if (dirFile.isFile()) {
+                String path = dirFile.getAbsolutePath();
+                insertFileIntoDB(dirFile, path);
+            }
+        }
     }
 }
