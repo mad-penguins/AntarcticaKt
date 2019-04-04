3dc70b5297fc191704ec7ed07e4b6dbdc10eb95b
Nick Mironov <nickfrom22nd@gmail.com>
2019-04-04 20:03:43 +0400

    SSL connection became optional

--- src/main/kotlin/utils/SessionFactoryUtil.kt
+++ src/main/kotlin/utils/SessionFactoryUtil.kt
@@ -10,17 +10,23 @@ import java.util.*
 
 object SessionFactoryUtil {
     private var sessionFactory: SessionFactory? = null
+    private var useSSL = false
 
-    fun getSessionFactory(id: Int, password: String): SessionFactory? {
+    fun getSessionFactory(id: Int, password: String, useSSL: Boolean = this.useSSL): SessionFactory? {
         if (sessionFactory == null) {
             try {
                 val prop = Properties()
-                prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/id$id?useSSL=true")
+                prop.setProperty(
+                        "hibernate.connection.url",
+                        "jdbc:mysql://localhost:3306/id$id${if (useSSL) "?useSSL=true" else ""}"
+                )
                 prop.setProperty("dialect", "org.hibernate.dialect.MariaDBDialect")
                 prop.setProperty("hibernate.connection.username", "id$id")
                 prop.setProperty("hibernate.connection.password", password)
-                prop.setProperty("hibernate.connection.verifyServerCertificate", "false") // TODO: delete this workaround when normally signed certificate will be present
-                prop.setProperty("hibernate.connection.requireSSL", "true")
+                if (useSSL) {
+                    prop.setProperty("hibernate.connection.verifyServerCertificate", "false") // TODO: delete this workaround when normally signed certificate will be present
+                    prop.setProperty("hibernate.connection.requireSSL", "true")
+                }
                 prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
                 prop.setProperty("show_sql", true.toString())
 
